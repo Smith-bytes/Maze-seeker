16066f98f94ac6d2607861dea874ae3e9b08aaba
立唐 謝 <tonyhsieh228@live.com>
2021-10-28 11:51:02 +0800

    na

--- Maze seeker/Maze_seeker.py
+++ Maze seeker/Maze_seeker.py
@@ -1,38 +1,49 @@
-# map seeker by Lee on 20211026
+
+import matplotlib.pyplot as plt 
+import numpy 
+try: 
+    f = open("maze.txt", "r") # opens .txt in read 
+    counter = sum(1 for lines in f) # creates a count of total number of element:2601 
+    sqrootc = int(numpy.sqrt(counter)) # sqrt counter to get dimension of maze 
+    f.close() # close file 
+except:
+    print("file does not exist")
+    exit(0)
+ 
+f = open("maze.txt", "r") # opens .txt in read 
+lines = f.readlines() # returns a list containing each line in the file as string 
+linesres = [ele == "True\n" for ele in lines] # convert str list to boolean list   
+
+for q in range(len(linesres)):
+    linesres[q]=int(linesres[q])
+
+f.close() # close file 
+ 
+arr = numpy.array(linesres) # converts the boolean list into a array 
+m = arr.reshape((sqrootc,sqrootc)) # give array 51Row by 51Column  
+
 maze_map = []
-maze_map = [
-           [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-           [1, 0, 1, 0, 1, 0, 0, 0, 0, 1], 
-           [1, 0, 1, 0, 1, 0, 0, 0, 0, 1],    
-           [1, 0, 0, 0, 1, 1, 1, 1, 0, 1],    
-           [1, 0, 1, 0, 0, 1, 0, 0, 0, 1],    
-           [1, 0, 1, 0, 0, 1, 0, 1, 0, 1],    
-           [1, 0, 1, 0, 0, 0, 0, 1, 0, 1],    
-           [1, 0, 1, 0, 0, 0, 0, 0, 0, 1],    
-           [1, 0, 1, 0, 0, 0, 0, 1, 0, 1],    
-           [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],]
+maze_map = m
 treasuremap = []
 routemap = []
 start = 1,1 #to be changed
-end = 1,5  #to be changed 
-
-        
+end = 1,47  #to be changed 
 
 def step_seeker(step): #To plot the step number in the corresponding location on treasure map (ex 2 after 1)
     global treasuremap
     for x in range(len(treasuremap)):
         for y in range(len(treasuremap[x])):
             if treasuremap[x][y] == step:
-                if x > 0 and treasuremap[x-1][y] == 0 and maze_map[x-1][y] == 0: #To check whether the location is empty, without any wall or previous walkthrough
+                if x > 0 and treasuremap[x-1][y] == 0 and maze_map[x-1, y] == 0 : #To check whether the location is empty, without any wall or previous walkthrough
                     treasuremap[x-1][y] = step+1
 
-                if y > 0 and treasuremap[x][y-1] == 0 and maze_map[x][y-1] == 0:
+                if y > 0 and treasuremap[x][y-1] == 0 and maze_map[x, y-1] == 0 :
                     treasuremap[x][y-1] = step+1
 
-                if x < len(treasuremap) -1 and treasuremap[x+1][y] == 0 and maze_map[x+1][y] == 0:
+                if x < len(treasuremap) -1 and treasuremap[x+1][y] == 0 and maze_map[x+1, y] == 0:
                     treasuremap[x+1][y] = step+1
 
-                if y < len(treasuremap[x])-1 and treasuremap[x][y+1] == 0 and maze_map[x][y+1] == 0:
+                if y < len(treasuremap[x])-1 and treasuremap[x][y+1] == 0 and maze_map[x, y+1] == 0:
                     treasuremap[x][y+1] = step+1
 
 def print_map(map):
@@ -49,19 +60,19 @@ def dir_path(step): #transfer treasure map to route map
         if t > 0 and treasuremap[t-1][a] == step -1: #to find a step which is -1 smaller than the current step at surrounding, until we reach starting point
             t -=1
            
-            routemap[t][a]= 1
+            routemap[t][a]= 2
             step -= 1
         elif a > 0 and treasuremap[t][a-1] == step -1:
             a -= 1
-            routemap[t][a]= 1
+            routemap[t][a]= 2
             step -= 1
         elif t < len(treasuremap) -1 and treasuremap[t+1][a] == step - 1:
             t += 1
-            routemap[t][a]= 1
+            routemap[t][a]= 2
             step -= 1
         elif a < len(treasuremap[t])-1 and treasuremap[t][a+1] == step -1:
             a += 1
-            routemap[t][a]= 1
+            routemap[t][a]= 2
             step -= 1
 
 
@@ -79,14 +90,14 @@ while treasuremap[end[0]][end[1]] == 0: #loop wont stop until the end point is r
     step += 1
     step_seeker(step)
 
-print_map(maze_map)
-
 print("pathing completed")
 print("total number of step:", step+1)
-
-print_map(treasuremap)
 
 t, a = end 
 dir_path(step+1)
 print("route map completed")
-print_map(routemap)
+
+G = numpy.array(routemap)
+m = m + G
+plt.imshow(m.T, cmap=plt.cm.viridis_r) #plot the transpose of m
+plt.show()
